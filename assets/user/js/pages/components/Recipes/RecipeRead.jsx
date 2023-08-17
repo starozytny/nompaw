24e0447c795b928f06f09448304e3b663c8ca1f3
Shanbora <chanbora@logilink.fr>
2023-08-17 11:46:38 +0200

    style rate

--- assets/user/js/pages/components/Recipes/RecipeRead.jsx
+++ assets/user/js/pages/components/Recipes/RecipeRead.jsx
@@ -157,7 +157,7 @@ export class RecipeRead extends Component {
                         }
                     </div>}
                     <div className="rating">
-                        <Rate disabled defaultValue={elem.rate} />
+                        <Rate defaultValue={elem.rate} />
                     </div>
 
                     {!isMobile
