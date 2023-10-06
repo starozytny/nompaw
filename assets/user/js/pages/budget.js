8e6ab396e8b0774c3efbb7961ef09161f8e56a62
Starozytny <starozytny@hotmail.fr>
2023-10-06 11:35:15 +0200

    init style planning

--- assets/user/js/pages/budget.js
+++ assets/user/js/pages/budget.js
@@ -1,5 +1,10 @@
 import '../../css/pages/budget.scss';
 
 import React from "react";
-import toastr from "toastr";
 import { createRoot } from "react-dom/client";
+import { Budget } from "@userPages/Budget/Budget";
+
+let el = document.getElementById("budget");
+if(el){
+    createRoot(el).render(<Budget />)
+}
