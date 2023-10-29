1855007cbb107d7f876ee814b00fd4f1d2e3cbd7
Starozytny <starozytny@hotmail.fr>
2023-10-29 12:55:51 +0100

    add santé cat

--- src/Controller/InternApi/Budget/InitController.php
+++ src/Controller/InternApi/Budget/InitController.php
@@ -39,6 +39,7 @@ class InitController extends AbstractController
             ['type' => TypeType::Expense, 'name' => "Transports", 'goal' => null,],
             ['type' => TypeType::Expense, 'name' => "Banque", 'goal' => null,],
             ['type' => TypeType::Expense, 'name' => "Voyage", 'goal' => null,],
+            ['type' => TypeType::Expense, 'name' => "Santé", 'goal' => null,],
             ['type' => TypeType::Income,  'name' => "Salaire", 'goal' => null,],
             ['type' => TypeType::Income,  'name' => "Cadeaux", 'goal' => null,],
             ['type' => TypeType::Income,  'name' => "Banque", 'goal' => null,],
