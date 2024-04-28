cd8721f2f571dbeb6c136163cd80ffdf4966ce10
Starozytny <starozytny@hotmail.fr>
2024-04-28 15:31:28 +0200

    group style

--- assets/user/js/pages/components/Aventures/Groupe/GroupeDelete.jsx
+++ assets/user/js/pages/components/Aventures/Groupe/GroupeDelete.jsx
@@ -12,37 +12,41 @@ import { Modal } from "@tailwindComponents/Elements/Modal";
 const URL_INDEX_ELEMENTS = 'user_aventures_groupes_index';
 const URL_DELETE_ELEMENT = 'intern_api_aventures_groupes_delete';
 
-export function GroupeDelete ({ context, id, name })
-{
-    let modalRef = useRef(null);
+export function GroupeDelete ({ context, id, name }) {
+	let modalRef = useRef(null);
 
-    const handleClick = () => { modalRef.current.handleClick(); }
+	const handleClick = () => {
+		modalRef.current.handleClick();
+	}
 
-    const handleDelete = () => {
-        let self = this;
+	const handleDelete = () => {
+		let self = this;
 
-        modalRef.current.handleUpdateFooter(<Button isLoader={true} type="danger">Confirmer la suppression</Button>);
-        axios({ method: "DELETE", url: Routing.generate(URL_DELETE_ELEMENT, {'id': id}), data: {} })
-            .then(function (response) {
-                location.href = Routing.generate(URL_INDEX_ELEMENTS);
-            })
-            .catch(function (error) { Formulaire.displayErrors(self, error); Formulaire.loader(false); })
-        ;
-    }
+		modalRef.current.handleUpdateFooter(<Button isLoader={true} type="red">Confirmer la suppression</Button>);
+		axios({ method: "DELETE", url: Routing.generate(URL_DELETE_ELEMENT, { 'id': id }), data: {} })
+			.then(function (response) {
+				location.href = Routing.generate(URL_INDEX_ELEMENTS);
+			})
+			.catch(function (error) {
+				Formulaire.displayErrors(self, error);
+				Formulaire.loader(false);
+			})
+		;
+	}
 
-    return <>
-        {context === "read"
-            ? <Button icon="trash" type="red" onClick={handleClick}>Supprimer</Button>
-            : <ButtonIcon icon="trash" type="none" onClick={handleClick}>Supprimer</ButtonIcon>
-        }
-        <Modal ref={modalRef} identifiant={`delete-groupe-${id}`} maxWidth={414} title="Supprimer le groupe"
-               content={<p>Etes-vous sûr de vouloir supprimer le groupe de randonnée : <b>{name}</b> ?</p>}
-               footer={<Button type="danger" onClick={handleDelete}>Confirmer la suppression</Button>} closeTxt="Annuler" />
-    </>
+	return <>
+		{context === "read"
+			? <Button icon="trash" type="red" onClick={handleClick}>Supprimer</Button>
+			: <ButtonIcon icon="trash" type="none" onClick={handleClick}>Supprimer</ButtonIcon>
+		}
+		<Modal ref={modalRef} identifiant={`delete-groupe-${id}`} maxWidth={414} title="Supprimer le groupe"
+			   content={<p>Etes-vous sûr de vouloir supprimer le groupe de randonnée : <b>{name}</b> ?</p>}
+			   footer={<Button type="red" onClick={handleDelete}>Confirmer la suppression</Button>} closeTxt="Annuler" />
+	</>
 }
 
 GroupeDelete.propTypes = {
-    context: PropTypes.string.isRequired,
-    id: PropTypes.string.isRequired,
-    name: PropTypes.string.isRequired,
+	context: PropTypes.string.isRequired,
+	id: PropTypes.string.isRequired,
+	name: PropTypes.string.isRequired,
 }
