6f170e0a4f919234184aa4dc0438361f8c530655
shanbora <chanbora@logilink.fr>
2024-08-13 17:01:01 +0200

    limit to 50

--- assets/user/js/pages/components/Aventures/Rando/RandoImages.jsx
+++ assets/user/js/pages/components/Aventures/Rando/RandoImages.jsx
@@ -251,8 +251,8 @@ export class RandoImages extends Component {
 						   Les photos seront automatiquement redimensionnées s'ils sont trop grandes/lourdes.
 					   </Alert>
                        <div className="mt-4">
-                           <InputFile ref={this.files} type="multiple" identifiant="files" valeur={files} accept="video/*,image/*" max={100} maxSize={95330000} {...params}>
-                               Photos (100 maximum par envoi)
+                           <InputFile ref={this.files} type="multiple" identifiant="files" valeur={files} accept="video/*,image/*" max={50} maxSize={95330000} {...params}>
+                               Photos (50 maximum par envoi)
                            </InputFile>
                        </div>
                    </>}
