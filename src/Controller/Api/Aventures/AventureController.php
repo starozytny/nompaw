3d8355865f84fb168ab2df550281e408a08ef060
shanbora <chanbora@logilink.fr>
2024-10-29 16:30:08 +0100

    group api

---
+++ src/Controller/Api/Aventures/AventureController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller\Api\Aventures;
+
+use App\Entity\Main\User;
+use App\Entity\Rando\RaGroupe;
+use App\Service\ApiResponse;
+use App\Service\Aventures\GroupService;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/api/aventures', name: 'api_aventures_')]
+class AventureController extends AbstractController
+{
+    #[Route('/list', name: 'list', options: ['expose' => true])]
+    public function list(ApiResponse $apiResponse, GroupService $groupService): JsonResponse
+    {
+        /** @var User $user */
+        $user = $this->getUser();
+        $groupes = $groupService->getList($user);
+
+        return $apiResponse->apiJsonResponse($groupes, RaGroupe::FORM);
+    }
+}
