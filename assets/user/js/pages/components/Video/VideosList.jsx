40f50892d9f22b41eb605afe3817df8e266f44ea
Shanbora <starozytny@hotmail.fr>
2025-12-30 15:18:42 +0100

    espace

--- assets/user/js/pages/components/Video/VideosList.jsx
+++ assets/user/js/pages/components/Video/VideosList.jsx
@@ -6,7 +6,7 @@ import { Alert } from "@tailwindComponents/Elements/Alert";
 import { VideosItem } from "@userPages/Video/VideosItem";
 
 export function VideosList ({ data, isAdmin, onModal }) {
-    return <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+    return <div className="my-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
         {data.length > 0
             ? data.map((elem) => {
                 return <VideosItem key={elem.filename} elem={elem} isAdmin={isAdmin} onModal={onModal} />;
